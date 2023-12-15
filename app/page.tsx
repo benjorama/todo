5abeb346c6b97bf7fecac1f4c97085df9b3dcd62
benjorama <benmacomb@gmail.com>
2023-12-15 16:07:32 -0700

    Adds some thoughts to the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <p>This is our /page.tsx file. It has a React component called Home() which renders this HTML. Very neat.</p>
       <h1>TODO</h1>
       <p>Do whatcha do!</p>
-      <p>I suppose this isn't much of a TODO list yet. I need to take some Next.js and React lessons. Be back shortly.</p>
+      <p>I suppose this isn't much of a TODO list yet. We should probably have the user enter some text or something....Lets see what we can do.</p>
     </div>
   )
 }
