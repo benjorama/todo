22ecf5530633bbb34e590932cee4f9e852944b56
benjorama <benmacomb@gmail.com>
2023-12-15 16:01:57 -0700

    Adds some more content to the Home component.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <p>This is our /page.tsx file. It has a React component called Home() which renders this HTML. Very neat.</p>
       <h1>TODO</h1>
       <p>Do whatcha do!</p>
+      <p>I suppose this isn't much of a TODO list yet. I need to take some Next.js and React lessons. Be back shortly.</p>
     </div>
   )
 }
