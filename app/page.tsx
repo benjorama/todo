5611c4c46b0bbe17e6c705adbe9361f9a380dc64
benjorama <benmacomb@gmail.com>
2023-12-19 17:50:51 -0700

    display current data and time

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-export default function Home() {
+import runQuery from "@/db/queries";
+
+async function getData() {
+  const rows = await runQuery()
+  return { rows }
+}
+
+export default async function Home() {
+  const data = await getData();
+  let now = data.rows[0].now.toString()
   return (
     <div>
       <h1>TODO</h1>
       <p>Do whatcha do!</p>
-      <form action="post" method="get">
+      <form method="post">
         <div>
           <label htmlFor="name">whatcha doin'?: </label>
           <input className="todo-input" type="text" name="name" id="name" required />
@@ -12,6 +21,7 @@ export default function Home() {
           <input type="submit" value="submit!" />
         </div>
       </form>
+      <p>{now}</p>
     </div>
   )
 }
