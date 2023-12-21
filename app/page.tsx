da102ebebd834e3b88f77c48d86050385e62a1fc
benjorama <benmacomb@gmail.com>
2023-12-21 16:08:44 -0700

    Logs raw form data

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,25 @@ async function getData() {
 }
 
 export default async function Home() {
+  async function createTodo(formData: FormData) {
+    'use server'
+
+    const rawFormData = {
+      todo: formData.get('todo')
+    }
+    console.log(rawFormData)
+  }
+
   const data = await getData();
   let now = data.rows[0].now.toString()
   return (
     <div>
       <h1>TODO</h1>
       <p>Do whatcha do!</p>
-      <form method="post">
+      <form method="post" action={createTodo}>
         <div>
-          <label htmlFor="name">whatcha doin'?: </label>
-          <input className="todo-input" type="text" name="name" id="name" required />
+          <label htmlFor="todo">whatcha doin'?: </label>
+          <input className="todo-input" type="text" name="todo" id="todo" required />
         </div>
         <div>
           <input type="submit" value="submit!" />
